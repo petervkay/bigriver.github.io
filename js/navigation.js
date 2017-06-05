fe5240d6c002946360724cee289b17f1c98a9756
petervkay <petervkay@gmail.com>
2017-06-05 13:36:32 -0500

    disfunctional attempt to fix forward/back buttons

--- js/navigation.js
+++ js/navigation.js
@@ -11,32 +11,13 @@ $(document).ready(function(){
 
 		} else { 								// case when anything but the home logo is clicked
 			var id =$(this).attr('id');
-			var url="/page-content/"+id+"/index.html";	
-			var title = id.charAt(0).toUpperCase() + id.slice(1); 
+			var url;
+			var title;
 		}
 
-		$.ajax({
-		    type: "GET",
-		    url: url,
-		    dataType: "html",
-		    success: function (html) {   //html returns the HTML contents of the requested page
-		    	if (isHome) {
-		        	$('#main').empty();
-		    	} else {
-		    		$('#main').html(html); 
-		    	}
-		        window.history.pushState("test",title,"/"+id);
-		        document.title=title;
+		defaultCreateInputs(url, title, id);
+		
 
-	    	}
-		});
-
-		window.onpopstate = function(e){
-    		if(e.state){
-	        document.getElementById("content").innerHTML = e.state.html;
-	        document.title = e.state.pageTitle;
-	    }
-};
 
 		$(".top-nav a").removeClass("active");
 		$(this).addClass("active");
@@ -44,4 +25,41 @@ $(document).ready(function(){
 	});
 });
 
+$(window).bind('popstate', function(){ //triggered on forward and back button events
+	console.log('backforward event');
+  	var id = window.location.pathname.split('/').filter(function(el){ return !!el; }).pop();  // takes the ID from the end of the URL
+  	var url;
+  	var title;
 
+  	defaultCreateInputs(url, title, id);
+});
+
+function defaultCreateInputs(url, title, id) {
+	var url="/page-content/"+id+"/index.html";
+	var title = id.charAt(0).toUpperCase() + id.slice(1);
+	loadHTML(url, title, id) 
+}
+
+function loadHTML(url, title, id) {
+	$.ajax({
+	    type: "GET",
+	    url: url,
+	    dataType: "html",
+	    success: function (html) {   //html returns the HTML contents of the requested page
+	    	if (isHome) {
+	        	$('#main').empty();
+	    	} else {
+	    		$('#main').html(html); 
+	    	}
+	        window.history.pushState("{page:1}",title,"/"+id);
+
+    	}
+	});
+
+	window.onpopstate = function(e){
+		if(e.state){
+        document.getElementById("content").innerHTML = e.state.html;
+        document.title = e.state.pageTitle;
+    	}
+	}
+}

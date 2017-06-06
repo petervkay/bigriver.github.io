7c86731514037d811761ecf55d9c645af9433752
petervkay <petervkay@gmail.com>
2017-06-06 11:01:08 -0500

    got navigation working!

--- js/navigation.js
+++ js/navigation.js
@@ -1,65 +1,63 @@
 $(document).ready(function(){
-	$(".top-nav a, .site-avatar").click(function(event){  //site-avatar is for the home logo
 
-		event.preventDefault();
-		var isHome = $(this).hasClass("site-avatar");
+	links = $(".top-nav a, a.home");
 
-		if (isHome) {  // case when the home logo is clicked
-			var url="/";
-			var title="{{site.title}}"
-			var id="";
+	function removeActiveClass(){
+    	$(links).removeClass('active');
+  	}
 
-		} else { 								// case when anything but the home logo is clicked
-			var id =$(this).attr('id');
-			var url;
-			var title;
-		}
+	function addActiveClass(elem){
+	    removeActiveClass();
+	    console.log(elem);
+	    var element = document.querySelector("#" + elem);
+	    element.classList.add('active');
+  	}
 
-		defaultCreateInputs(url, title, id);
-		
-
-
-		$(".top-nav a").removeClass("active");
-		$(this).addClass("active");
-
-	});
-});
-
-$(window).bind('popstate', function(){ //triggered on forward and back button events
-	console.log('backforward event');
-  	var id = window.location.pathname.split('/').filter(function(el){ return !!el; }).pop();  // takes the ID from the end of the URL
-  	var url;
-  	var title;
-
-  	defaultCreateInputs(url, title, id);
-});
-
-function defaultCreateInputs(url, title, id) {
-	var url="/page-content/"+id+"/index.html";
-	var title = id.charAt(0).toUpperCase() + id.slice(1);
-	loadHTML(url, title, id) 
-}
-
-function loadHTML(url, title, id) {
-	$.ajax({
-	    type: "GET",
-	    url: url,
-	    dataType: "html",
-	    success: function (html) {   //html returns the HTML contents of the requested page
-	    	if (isHome) {
-	        	$('#main').empty();
-	    	} else {
-	    		$('#main').html(html); 
-	    	}
-	        window.history.pushState("{page:1}",title,"/"+id);
-
-    	}
-	});
-
-	window.onpopstate = function(e){
-		if(e.state){
-        document.getElementById("content").innerHTML = e.state.html;
-        document.title = e.state.pageTitle;
-    	}
+	function requestContent(url){
+		$("#main").load(url);
 	}
-}
+	
+
+	$(links).click(function(e) {
+		
+		  	e.preventDefault();
+
+			isHome = $(this).hasClass('home');
+			console.log('ishome: ' + isHome);
+
+			if (isHome==false) {		
+		    	var data = e.target.getAttribute('data-name');
+		   		var url = "/page-content/"+data+"/index.html";
+			    addActiveClass(data);
+			    history.pushState(data, null, "/"+data);
+			   // updateText(data);
+			    requestContent(url);
+			    document.title = "Big River Web Design | " + data;
+			} else {
+				removeActiveClass();
+				$("#main").empty();
+				history.pushState("home", null, "/")
+				document.title = "Big River Web Design"
+			}
+	
+	    	e.stopPropagation();
+	});
+
+	window.addEventListener('popstate', function(e){
+
+    	var data = e.state;
+
+    	if (data!="home") {
+	    	var url = "/page-content/"+data+"/index.html";
+	    	addActiveClass(data);
+	    	requestContent(url);
+	      	document.title = "Big River Web Design | " + data;
+	    } else {
+	    	removeActiveClass();
+			$("#main").empty();
+			document.title = "Big River Web Design";
+	    }
+
+
+	});
+});

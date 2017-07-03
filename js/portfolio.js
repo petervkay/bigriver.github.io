c34b37bac138df8e246379bd6ec9e573eab5e5b8
Andrew S. Lentz <Andrew.S.Lentz@gmail.com>
2017-07-03 11:58:03 -0500

    relocated some functions to portfolio.js and got the tag and site selecting via url feature up and running

--- js/portfolio.js
+++ js/portfolio.js
@@ -50,3 +50,34 @@
 						});
 					}
 				}
+
+				function eventFire(el, etype){
+				  if (el.fireEvent) {
+				    el.fireEvent('on' + etype);
+				  } else {
+				    var evObj = document.createEvent('Events');
+				    evObj.initEvent(etype, true, false);
+				    el.dispatchEvent(evObj);
+				  }
+				}
+
+				function getParameterByName(name, url) {
+				    if (!url) url = window.location.href;
+				    name = name.replace(/[\[\]]/g, "\\$&");
+				    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+				        results = regex.exec(url);
+				    if (!results) return null;
+				    if (!results[2]) return '';
+				    return decodeURIComponent(results[2].replace(/\+/g, " "));
+				}
+				if (getParameterByName('site')) {
+					eventFire(document.getElementById(getParameterByName('site')), 'click');
+				}
+				if (getParameterByName('btag')) {
+					console.log(getParameterByName('btag'));
+					eventFire(document.getElementById(getParameterByName('btag')), 'click');
+				}
+				if (getParameterByName('ptag')) {
+					console.log(getParameterByName('ptag'));
+					eventFire(document.getElementById(getParameterByName('ptag')), 'click');
+				}

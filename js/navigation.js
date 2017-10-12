dc8aded07dc0e80f4b53eb76231538016e6e0c0c
Andrew S. Lentz <Andrew.S.Lentz@gmail.com>
2017-10-12 11:39:27 -0500

    further consolidated pagescroll code

--- js/navigation.js
+++ js/navigation.js
@@ -64,25 +64,33 @@ $(document).ready(function(){
 
 	// pageScroll code
 
+	function downScroll(nextPage) {
+		$('#main-container').hide();
+		addActiveClass(nextPage);
+		history.pushState(nextPage, null, "{{site.baseurl}}/" + nextPage + "/");
+		requestContent("{{site.baseurl}}/page-content/"+ nextPage +"/index.html");
+		document.title = "Big River Web Design | " + nextPage;
+		removeClassFixed();
+	}
+
+	function upScroll(previousPage) {
+		$('#main-container').fadeOut();
+		addActiveClass(previousPage);
+		history.pushState(previousPage, null, "{{site.baseurl}}/" + previousPage + "/");
+		requestContent("{{site.baseurl}}/page-content/" + previousPage + "/index.html");
+		document.title = "Big River Web Design | " + previousPage;
+		$('html,body').scrollTop(80);
+	}
+
 	$(window).scroll(function() {
 		console.log($(window).scrollTop());
 		if (location.origin + '{{site.baseurl}}/' == location.href) {
 			if ($(window).scrollTop() + $(window).height() == $(document).height()) {
-				$('#main-container').hide();
-				addActiveClass("about");
-				history.pushState("about", null, "{{site.baseurl}}/about/");
-				requestContent("{{site.baseurl}}/page-content/about/index.html");
-				document.title = "Big River Web Design | about";
-				removeClassFixed();
+				downScroll('about');
 			}
 		} else if (location.origin + '{{site.baseurl}}/about/' == location.href) {
 	    if ($(window).scrollTop() + $(window).height() == $(document).height()) {
-				$('#main-container').hide();
-	      addActiveClass("services");
-	      history.pushState("services", null, "{{site.baseurl}}/services/");
-	      requestContent("{{site.baseurl}}/page-content/services/index.html");
-	      document.title = "Big River Web Design | services";
-	      removeClassFixed();
+				downScroll('services');
 	    } else if ($(window).scrollTop() == 0) {
 				$('#main-container').fadeOut();
 	      removeActiveClass();
@@ -93,61 +101,25 @@ $(document).ready(function(){
 	    }
 	  } else if (location.origin + '{{site.baseurl}}/services/' == location.href) {
 	    if ($(window).scrollTop() + $(window).height() == $(document).height()) {
-				$('#main-container').hide();
-	      addActiveClass("portfolio");
-	      history.pushState("portfolio", null, "{{site.baseurl}}/portfolio/");
-	      requestContent("{{site.baseurl}}/page-content/portfolio/index.html");
-	      document.title = "Big River Web Design | portfolio";
-	      removeClassFixed();
+				downScroll('portfolio');
 	    } else if ($(window).scrollTop() == 0) {
-				$('#main-container').fadeOut();
-	      addActiveClass("about");
-	      history.pushState("about", null, "{{site.baseurl}}/about/");
-	      requestContent("{{site.baseurl}}/page-content/about/index.html");
-	      document.title = "Big River Web Design | about";
-	      $('html,body').scrollTop(80);
+				upScroll('about');
 	    }
 	  } else if (location.origin + '{{site.baseurl}}/portfolio/' == location.href) {
 	    if ($(window).scrollTop() + $(window).height() == $(document).height()) {
-				$('#main-container').hide();
-	      addActiveClass("members");
-	      history.pushState("members", null, "{{site.baseurl}}/members/");
-	      requestContent("{{site.baseurl}}/page-content/members/index.html");
-	      document.title = "Big River Web Design | members";
-	      removeClassFixed();
+				downScroll('members');
 	    } else if ($(window).scrollTop() == 0) {
-				$('#main-container').fadeOut();
-	      addActiveClass("services");
-	      history.pushState("services", null, "{{site.baseurl}}/services/");
-	      requestContent("{{site.baseurl}}/page-content/services/index.html");
-	      document.title = "Big River Web Design | services";
-				console.log($('#main-container').height()+100);
-				$('html,body').scrollTop(80);
+				upScroll('services');
 	    }
 	  } else if (location.origin + '{{site.baseurl}}/members/' == location.href) {
 	    if ($(window).scrollTop() + $(window).height() == $(document).height()) {
-				$('#main-container').hide();
-	      addActiveClass("contact");
-	      history.pushState("contact", null, "{{site.baseurl}}/contact/");
-	      requestContent("{{site.baseurl}}/page-content/contact/index.html");
-	      document.title = "Big River Web Design | contact";
-	      removeClassFixed();
+				downScroll('contact');
 	    } else if ($(window).scrollTop() == 0) {
-				$('#main-container').fadeOut();
-	      addActiveClass("portfolio");
-	      history.pushState("portfolio", null, "{{site.baseurl}}/portfolio/");
-	      requestContent("{{site.baseurl}}/page-content/portfolio/index.html");
-	      document.title = "Big River Web Design | portfolio";
-	      $('html,body').scrollTop(80);
+				upScroll('portfolio');
 	    }
 	  } else if (location.origin + '{{site.baseurl}}/contact/' == location.href) {
 	    if ($(window).scrollTop() == 0) {
-				$('#main-container').hide();
-	      addActiveClass("members");
-	      history.pushState("members", null, "{{site.baseurl}}/members/");
-	      requestContent("/page-content/members/index.html");
-	      document.title = "Big River Web Design | members";
-	      $('html,body').scrollTop(80);
+				upScroll('members');
 	    }
 	  }
 	})
